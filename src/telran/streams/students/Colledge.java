cec9040199d6db593bfbf2dca5eeb8cdcc59437e
Nadelele <nadele@gmail.com>
2024-05-20 12:42:11 +0300

    Moved index variable to iterator

--- src/telran/streams/students/Colledge.java
+++ src/telran/streams/students/Colledge.java
@@ -6,33 +6,33 @@ import java.util.NoSuchElementException;
 
 public class Colledge implements Iterable<Student> {
 	Student[] students;
-	private int index;
-	
+
 	public Colledge(Student[] students) {
 		this.students = Arrays.copyOf(students, students.length);
-		this.index = 0;
-	}
-private class ColledgeIterator implements Iterator<Student> {
-
-	@Override 
-	public boolean hasNext() {
-		return index < students.length;
 	}
 
-	@Override
-	public Student next() {
-		if(!hasNext()) {
-			throw new NoSuchElementException();
+	private class ColledgeIterator implements Iterator<Student> {
+		private int index;
+
+		@Override
+		public boolean hasNext() {
+			return index < students.length;
 		}
-		return students[index++];
+
+		@Override
+		public Student next() {
+			if (!hasNext()) {
+				throw new NoSuchElementException();
+			}
+			return students[index++];
+		}
+
 	}
-	
-}
+
 	@Override
 	public Iterator<Student> iterator() {
-		
+
 		return new ColledgeIterator();
 	}
 
-	
 }
